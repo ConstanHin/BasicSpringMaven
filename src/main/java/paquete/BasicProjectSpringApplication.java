192deda492eca989c4a5c53815e836dfb96e6ac7
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-11 12:29:50 +0200

    basic spring maven project

---
+++ src/main/java/paquete/BasicProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package paquete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicProjectSpringApplication.class, args);
+	}
+
+}
